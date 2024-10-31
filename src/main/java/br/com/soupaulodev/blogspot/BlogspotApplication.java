d4ae27cddca5e34dae4e2beb1a7d93e2e0b7dff3
Paulo Marques <paulomarques.rs1@gmail.com>
2024-10-31 15:51:41 -0300

    feat(initial): initial project setup
    Initial commit setting up the project structure, dependencies, and basic configuration for a blogspot application. Includes basic testing and Dockerfile for future development.

---
+++ src/main/java/br/com/soupaulodev/blogspot/BlogspotApplication.java
@@ -0,0 +1,13 @@
+package br.com.soupaulodev.blogspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogspotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/soupaulodev/blogspot/BlogspotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soupaulodev.blogspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogspotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
